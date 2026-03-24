27764f003b37f60fac0f45d7709824daa09be474
Nataliia Verchyn <ujlscm@ukr.net>
2026-03-24 18:45:30 +0000

    опис змін

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,46 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const pool = require("./db");
+const jwt = require("jsonwebtoken");
+const authRouter = require("./routes/auth");
+const roomsRouter = require("./routes/rooms");
+const friendsRouter = require("./routes/friends");
+const uploadRouter = require("./routes/upload");
+const adminRouter = require("./routes/admin");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static("uploads"));
+
+app.use("/auth", authRouter);
+app.use("/rooms", roomsRouter);
+app.use("/friends", friendsRouter);
+app.use("/upload", uploadRouter);
+app.use("/admin", adminRouter);
+
 const server = http.createServer(app);
-const io = new Server(server, {
-cors: { origin: "*" }
+const io = new Server(server, { cors: { origin: "*" } });
+
+io.use((socket, next) => {
+  try {
+    socket.user = jwt.verify(socket.handshake.auth.token, "supersecret");
+    next();
+  } catch {
+    next(new Error("Unauthorized"));
+  }
 });
-// SOCKET CHAT
+
 io.on("connection", (socket) => {
-console.log("User connected");
-socket.on("join", (room) => {
-socket.join(room);
+  socket.on("joinRoom", (roomId) => socket.join("room_" + roomId));
+  socket.on("roomMessage", async ({ roomId, content }) => {
+    await pool.query(
+      "INSERT INTO messages (sender_id, room_id, content) VALUES ($1,$2,$3)",
+      [socket.user.id, roomId, content]
+    );
+    io.to("room_" + roomId).emit("roomMessage", { content });
+  });
 });
-socket.on("message", ({ room, message }) => {
-io.to(room).emit("message", message);
-});
-});
-app.get("/", (req, res) => {
-res.send("Chat server running");
-});
-server.listen(3000, () => console.log("Server running"));
+
+server.listen(3000, () => console.log("Backend running on 3000"));
